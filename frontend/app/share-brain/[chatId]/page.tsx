27008896c95efb327d9e539eac9732af560e33af
unifyqReactDev <reactdevlm@gmail.com>
2023-09-05 19:38:28 +1000

    fix: resize min height to 90vh

--- frontend/app/share-brain/[chatId]/page.tsx
+++ frontend/app/share-brain/[chatId]/page.tsx
@@ -17,7 +17,7 @@ const SelectedChatPage = (): JSX.Element => {
 
   return (
     <main className="flex flex-col w-full pt-10" data-testid="chat-page">
-      <section className="flex flex-col flex-1 items-center w-full h-full min-h-[70vh]">
+      <section className="flex flex-col flex-1 items-center w-full h-full min-h-[90vh]">
         <ChatHeader />
         <div className="flex-1 flex flex-col mt-8 w-full shadow-md dark:shadow-primary/25 hover:shadow-xl transition-shadow rounded-xl overflow-hidden bg-white dark:bg-black border border-black/10 dark:border-white/25 p-12 pt-10 max-h-[80vh]">
           <div className="flex flex-col flex-1 overflow-hidden">
