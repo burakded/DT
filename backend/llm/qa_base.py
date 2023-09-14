2cf6fedf1c3f429e94e4648581ba0d02a1e99b4e
vitalikshuliak <vitalikshuliak3@gmail.com>
2023-09-14 10:02:39 -0500

    fix: change bot name to 'Digital Twin' in default prompt

--- backend/llm/qa_base.py
+++ backend/llm/qa_base.py
@@ -34,7 +34,7 @@ from .base import BaseBrainPicking
 from .prompts.CONDENSE_PROMPT import CONDENSE_QUESTION_PROMPT
 
 logger = get_logger(__name__)
-QUIVR_DEFAULT_PROMPT = "Your name is KKK. You're a helpful assistant.  If you don't know the answer, just say that you don't know, don't try to make up an answer."
+QUIVR_DEFAULT_PROMPT = "Your name is a Digital Twin. You're a helpful assistant.  If you don't know the answer, just say that you don't know, don't try to make up an answer."
 
 
 class QABaseBrainPicking(BaseBrainPicking):
