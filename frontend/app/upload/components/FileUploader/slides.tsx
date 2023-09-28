29c279aed8d67b30d92ccfcaa369c12e77703f74
vitalikshuliak <vitalikshuliak3@gmail.com>
2023-09-28 15:28:32 +0300

    fix: CC make file upload perfect

--- frontend/app/upload/components/FileUploader/slides.tsx
+++ frontend/app/upload/components/FileUploader/slides.tsx
@@ -146,7 +146,7 @@ export const Slides = (): JSX.Element => {
   ]
 
   useEffect(() => {
-    if (!isPending && selectedIndex !== 0) {
+    if (isPending === false) {
       setSelectedIndex((prev) => prev + 1);
     }
   }, [isPending])
@@ -175,13 +175,13 @@ export const Slides = (): JSX.Element => {
       <div className="flex justify-between mt-4">
         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={() => { setSelectedIndex((prev) => prev - 1) }}
-          disabled={files.length !== 0}
+          disabled={files.length !== 0 || isPending === true}
         >
           Prev
         </button>
         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
           onClick={() => { setSelectedIndex((next) => next + 1) }}
-          disabled={files.length !== 0}
+          disabled={files.length !== 0 || isPending === true}
         >
           Next
         </button>
