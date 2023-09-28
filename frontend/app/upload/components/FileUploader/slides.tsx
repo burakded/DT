acff50851f3bdab2bd8a9a242554e31561853847
vitalikshuliak <vitalikshuliak3@gmail.com>
2023-09-28 13:48:16 +0300

    fix: auto move after upload

--- frontend/app/upload/components/FileUploader/slides.tsx
+++ frontend/app/upload/components/FileUploader/slides.tsx
@@ -1,5 +1,5 @@
 import { AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Carousel } from 'react-responsive-carousel';
 
@@ -85,15 +85,83 @@ export const Slides = (): JSX.Element => {
         </div>
       </section>
     );
-  }
+  };
+  const uploadContent = (): JSX.Element => {
+    return (
+      <section
+        {...getRootProps()}
+        className="w-full outline-none flex flex-col gap-10 items-center justify-center px-6 py-3"
+      >
+        <div className="flex flex-col sm:flex-row max-w-3xl w-full items-center gap-5">
+          <div className="flex-1 w-full">
+            <Card className="h-52 flex justify-center items-center">
+              <input {...getInputProps()} />
+              <div className="text-center p-6 max-w-sm w-full flex flex-col gap-5 items-center">
+                {isDragActive ? (
+                  <p className="text-blue-600">{t("drop", { ns: "upload" })}</p>
+                ) : (
+                  <button
+                    onClick={open}
+                    className="opacity-50 h-full cursor-pointer hover:opacity-100 hover:underline transition-opacity"
+                  >
+                    {t("dragAndDrop", { ns: "upload" })}
+                  </button>
+                )}
+              </div>
+            </Card>
+          </div>
+
+          {files.length > 0 && (
+            <div className="flex-1 w-full">
+              <Card className="h-52 py-3 overflow-y-auto">
+                {files.length > 0 ? (
+                  <AnimatePresence mode="popLayout">
+                    {files.map((file) => (
+                      <FileComponent
+                        key={`${file.name} ${file.size}`}
+                        file={file}
+                        setFiles={setFiles}
+                      />
+                    ))}
+                  </AnimatePresence>
+                ) : null}
+              </Card>
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col items-center justify-center mb-6">
+          <Button isLoading={isPending} onClick={() => void uploadAllFiles()}>
+            {isPending ? t("uploadingButton") : t("uploadButton")}
+          </Button>
+        </div>
+      </section>
+    );
+  };
+
+  const contents: JSX.Element[] = [
+    slidesContent(),
+    uploadContent()
+  ]
+
+  useEffect(() => {
+    if (!isPending) {
+      setSelectedIndex((prev) => prev + 1);
+    }
+  }, [isPending])
 
   return (
     <>
       <div className="flex justify-between mt-4">
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => { setSelectedIndex((prev) => prev - 1) }}>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          onClick={() => { setSelectedIndex((prev) => prev - 1) }}
+          disabled={files.length !== 0}
+        >
           Prev
         </button>
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => { setSelectedIndex((next) => next + 1) }}>
+        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          onClick={() => { setSelectedIndex((next) => next + 1) }}
+          disabled={files.length !== 0}
+        >
           Next
         </button>
       </div>
@@ -108,7 +176,7 @@ export const Slides = (): JSX.Element => {
             slideTitles.map((title, index) => (
               <div key={index}>
                 <a>{title}</a>
-                {slidesContent()}
+                {contents[index]}
               </div>
             ))
           }
