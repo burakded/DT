5e79390869cefe40d51402ddbf44431896cc4520
andriikotsuba <upwork2288@gmail.com>
2023-10-17 23:26:27 +1100

    fix: changed FE url

--- backend/middlewares/cors.py
+++ backend/middlewares/cors.py
@@ -1,10 +1,7 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
-    "https://digitaltwin-50.educatedapp.com",
-    "http://147.182.142.200",
-    "http://147.182.142.200:3000",
-    "http://147.182.142.200:3001",
+    "https://digitaltwin.educatedapp.com/",
     "https://quivr.app",
     "https://www.quivr.app",
     "http://quivr.app",
