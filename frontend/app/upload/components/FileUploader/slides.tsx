5ee1c08db738315500eedacf786c1ee3dccd8e7d
Burak <bdedeoglu@educatedc.com>
2024-10-11 16:35:45 +0300

    ui fixed

--- frontend/app/upload/components/FileUploader/slides.tsx
+++ frontend/app/upload/components/FileUploader/slides.tsx
@@ -34,7 +34,7 @@ export const Slides = ({ setIsModalOpen }: SlidesProps): JSX.Element => {
   };
 
   const slideTitles = [
-    "Quadrant 11. Personal information",
+    "Quadrant 1. Personal Information",
     "Quadrant 2. Professional information",
     "Quadrant 3. Thought Leadership",
     "Quadrant 4. Legacy",
